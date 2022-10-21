04b072998b70298eeebb3c38431a82b0c13976ff
CutzuDev <alexfarkas05@gmail.com>
2022-10-21 16:20:07 +0300

    Update

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -2,18 +2,18 @@ import "./CardItem.css";
 import ItemImg from "../assets/shopitem.png";
 import { Button } from "@mui/material";
 
-function CardItem({ cardSubTitle, cardPrice }) {
+function CardItem({ cardSubTitle, cardPrice, cardLink }) {
   return (
     <div className="card">
       <img src={ItemImg} className="carditem__img" />
       <h1 className="card__title">Storm V2</h1>
       <h3 className="card__sub-title">{cardSubTitle}</h3>
       <span className="card__price">{cardPrice}</span>
-      {/* <a href="" className="card__button--link"> */}
-      <Button variant="outlined" className="card__button" color="primary">
-        <h3 className="card__button--text">Buy Now!</h3>
-      </Button>
-      {/* </a> */}
+      <a href={cardLink} className="card__button--link">
+        <Button variant="outlined" className="card__button" color="primary">
+          <h3 className="card__button--text">Buy Now!</h3>
+        </Button>
+      </a>
     </div>
   );
 }
